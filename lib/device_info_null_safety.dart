bfda1f8a07c8c19cd7b4808063abc9115d098a76
Divit Vaghani <divitvaghani9192@gmail.com>
2022-09-10 14:43:51 +0530

    added comment & documentation

--- lib/device_info_null_safety.dart
+++ lib/device_info_null_safety.dart
@@ -2,36 +2,48 @@ import 'package:device_info_null_safety/memory_info_enum.dart';
 
 import 'device_info_null_safety_platform_interface.dart';
 
+///main class exposed to user for different device information
 class DeviceInfoNullSafety {
   final DeviceInfoNullSafetyPlatform _instance =
       DeviceInfoNullSafetyPlatform.instance;
 
+  /// get Battery info with this getter
   Future<Map<String, dynamic>> get batteryInfo => _instance.getBatteryInfo();
 
+  /// get ABi info with this getter
   Future<Map<String, dynamic>> get abiInfo => _instance.getAbiInfo();
 
+  /// get Sensor info with this getter
   Future<List<Map<String, dynamic>>> get sensorInfo =>
       _instance.getSensorInfo();
 
+  /// get Memory info with this getter
   Future<Map<String, dynamic>> memoryInfo(
           {required MemoryInfoUnit memoryInfoUnit}) =>
       _instance.getMemoryInfo(memoryInfoUnit: memoryInfoUnit);
 
+  /// get Finger Print info with this getter
   Future<Map<String, dynamic>> get fingerPrintInfo =>
       _instance.getFingerprintInfo();
 
+  /// get System info with this getter
   Future<Map<String, dynamic>> get systemInfo => _instance.getSystemInfo();
 
+  /// get Display info with this getter
   Future<Map<String, dynamic>> get displayInfo => _instance.getDisplayInfo();
 
+  /// get Config info with this getter
   Future<Map<String, dynamic>> get configInfo => _instance.getConfigInfo();
 
   ///As for Now Don't use SimInfo Getter
   // Future<Map<String, dynamic>> get simInfo => _instance.getSimInfo();
-
+  /// get NFC info with this getter
   Future<Map<String, dynamic>> get nfcInfo => _instance.getNfcInfo();
 
+  /// get Network info with this getter
   Future<Map<String, dynamic>> get networkInfo => _instance.getNetworkInfo();
 
+  /// get Location info with this getter
+  /// to use these getter you must use permission handler to get current location of user
   Future<Map<String, dynamic>> get locationInfo => _instance.getLocationInfo();
 }
--- lib/memory_info_enum.dart
+++ lib/memory_info_enum.dart
@@ -1,1 +1,2 @@
+/// Enum to get Memory info in different unit
 enum MemoryInfoUnit { mb, gb, kb, bytes }
